f97103a1e8f501935fd7a3840acaf596c6843336
OlgaSivyuk <olchik0308@yandex.ru>
2022-04-27 13:49:28 +0300

    connect api for profile

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,19 @@
+import React from 'react';
+
 
 function Cards() {
   return (
-    <template className="template">
-        <article className="place">
-            <img alt="" className="place__image" src="." />
-            <div className="place__info">
-                <h2 className="place__name"></h2>
-                <div className="place__reaction">
-                    <button type="button" className="place__like" aria-label="Нравится"></button>
-                    <span className="place__like-count"></span>
-                </div>
-            </div>
-            <button type="button" className="place__delete" arial-label="Удалить"></button>
-        </article>
-    </template>
+    <article className="place">
+      <img alt="" className="place__image" src="." />
+      <div className="place__info">
+        <h2 className="place__name"></h2>
+        <div className="place__reaction">
+          <button type="button" className="place__like" aria-label="Нравится"></button>
+          <span className="place__like-count"></span>
+        </div>
+      </div>
+      <button type="button" className="place__delete" arial-label="Удалить"></button>
+    </article>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,46 @@
+import React, {useState, useEffect} from 'react';
 import avatar from '../images/avatar.jpg';  // заглушка
+import {api} from '../utils/Api.js'
 import Cards from './Cards.js'
 
+
 function Main({ onEditAvatar, onEditProfile, onAddPlace  }) {
+
+    const [userName, setUserName] = useState('');
+    const [userDescription, setUserDescription] = useState('');
+    const [userAvatar, setUserAvatar] = useState('');
+
+    useEffect(() => {
+        api.getProfile()
+          .then(res => {
+            setUserName(res.name);
+            setUserDescription(res.about);
+            setUserAvatar(res.avatar);
+          })
+          .catch(err => console.log(err));
+      }, []);
+
+
+
+
     return (
       <main className="content">
         <section className="profile">
           <div className="profile__avatar-img">
-            <img src={avatar} alt="Фото профеля" className="profile__avatar"/>
+            <img src={userAvatar} alt="Фото профиля" className="profile__avatar"/>
             <button title="Загрузить аватар" className="profile__avatar-button" onClick={onEditAvatar}></button>
           </div>
           <div className="profile__info">
             <div className="profile__top-row">
-              <h1 className="profile__name">Жак-Ив Кусто</h1>
+              <h1 className="profile__name">{userName}</h1>
               <button type="button" id="open_popup" className="profile__add-info" aria-label="Редактировать профиль" onClick={onEditProfile}></button>
             </div>
-            <p className="profile__bio">Исследователь океана</p>
+            <p className="profile__bio">{userDescription}</p>
           </div>
           <button type="button" className="profile__add-place" aria-label="Добавить фото" onClick={onAddPlace}></button>
         </section>
         <section className="places">
+            
         </section>
       </main>
     );
